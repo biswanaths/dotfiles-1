22bb1a0cd591003e75da9319d7c72b3b196abae1
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-21 19:02:42 -0800

    More fishes

--- fish/config.fish
+++ fish/config.fish
@@ -1,69 +1,47 @@
 # ajh's config.fish
+
+# Environment Variables {{{1
 set -x PATH ~/.bin /usr/local/bin /usr/local/sbin $PATH
 set BROWSER open
 set -g -x fish_greeting ''
 set -g -x EDITOR vim
+set -g -x ARCHFLAGS "-arch x86_64"
 
+# Key mappings {{{1
 function fish_user_key_bindings
     bind \cn 'accept-autosuggestion'
 end
 
+# Aliases {{{1
+function sudo!!
+    eval sudo $history[1]
+end
 function c; clear; end
 function ef; vim ~/.config/fish/config.fish; end
 function gst; git status; end
 function j; z $argv; end
+function upd; brew update; brew upgrade; brew cleanup; end
 alias ls "ls -AG"
 
-# Prompt (slightly modified from sjl's fish prompt)
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set gray (set_color -o black)
+# Prompt {{{1
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
 
-function git_prompt
-    if git root >/dev/null 2>&1
-        set_color normal
-        printf ' on '
-        set_color magenta
-        printf '%s' (git currentbranch ^/dev/null)
-        set_color green
-#        git_prompt_status
-        set_color normal
-    end
-end
-
-function prompt_pwd --description 'Print the current working directory, shortend to fit the prompt'
-    echo $PWD | sed -e "s|^$HOME|~|"
-end
-
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+ 
 function fish_prompt
-    set last_status $status
-    z --add "$PWD"
-    echo
-    set_color magenta
-    printf '%s' (whoami)
-    set_color normal
-    printf ' at '
-    set_color yellow
-    printf '%s' (hostname|cut -d . -f 1)
-    set_color normal
-    printf ' in '
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
-    git_prompt
-
-    echo
-
-    if test $last_status -eq 0
-        set_color white -o
-        printf '><((°> '
-    else
-        set_color red -o
-        printf '[%d] ><((ˣ> ' $last_status
-    end
-
-    set_color normal
+        set last_status $status
+        set_color $fish_color_cwd
+        printf '%s' (prompt_pwd)
+        set_color normal
+        printf '%s ' (__fish_git_prompt)
+       set_color normal
 end
-# vim: ft=fish
+# vim: ft=fish fdm=marker fen
