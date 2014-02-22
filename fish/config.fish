ee8a4206bbd0385629fd119751e5e99bf388d445
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-21 19:06:07 -0800

    more fish

--- fish/config.fish
+++ fish/config.fish
@@ -30,12 +30,12 @@ set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch yellow
 
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_dirtystate 'λ'
 set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_stashstate '*'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
- 
+
 function fish_prompt
         set last_status $status
         set_color $fish_color_cwd
