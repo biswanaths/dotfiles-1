74aa30443d0c68d36abb149a57496ea0137c10ec
Akshay Hegde <akshay.hegde17@gmail.com>
2014-02-23 01:59:25 -0800

    Completed moving back to zsh

---
+++ .zsh/styles.zsh
@@ -0,0 +1,62 @@
+# Styles for completions
+# Mostly taken or edited from prezto
+
+autoload -Uz compinit && compinit -i
+
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path '~/.cache/.zcompcache'
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+zstyle ':completion:*:*:*:*:*' menu select
+zstyle ':completion:*:matches' group 'yes'
+zstyle ':completion:*:options' description 'yes'
+zstyle ':completion:*:options' auto-description '%d'
+zstyle ':completion:*:corrections' format ' %F{green}-- %d (errors: %e) --%f'
+zstyle ':completion:*:descriptions' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*:messages' format ' %F{purple} -- %d --%f'
+zstyle ':completion:*:warnings' format ' %F{red}-- no matches found --%f'
+zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+zstyle ':completion:*' format ' %F{yellow}-- %d --%f'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' verbose yes
+
+# Fuzzy matches
+zstyle ':completion:*' completer _complete _match _approximate
+zstyle ':completion:*:match:*' original only
+zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+# Directory completion styles
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*' squeeze-slashes true
+
+# Increase the number of errors based on the length of the typed word.
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3))numeric)'
+
+# Don't complete unavailable commands.
+zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+
+# Environment variables
+zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+
+# Ignore multiple entries
+zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
+zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+
+# kill
+zstyle ':completion:*:*:*:*:processes' command 'ps -u $USER -o pid,user,comm -w'
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
+zstyle ':completion:*:*:kill:*' menu yes select
+zstyle ':completion:*:*:kill:*' force-list always
+zstyle ':completion:*:*:kill:*' insert-ids single
+
+# man pages
+zstyle ':completion:*:manuals' separate-sections true
+zstyle ':completion:*:manuals.(^1*)' insert-sections true
+
+# History
+zstyle ':completion:*:history-words' stop yes
+zstyle ':completion:*:history-words' remove-all-dups yes
+zstyle ':completion:*:history-words' list false
+zstyle ':completion:*:history-words' menu yes
--- .zshrc
+++ .zshrc
@@ -1,64 +1,35 @@
 # ajh17's ~/.zshrc
 # Source externals {{{1
-if [[ -s "${ZDOTDIR:-$HOME}/.zprezto/init.zsh" ]]; then
-  source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
-fi
+source ~/.zsh/styles.zsh
+source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/zsh-history-substring-search/zsh-history-substring-search.zsh
+source ~/.zsh/steeef.zsh-theme
 source ~/.zsh/aliases
+eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)"
 
 # Variables {{{1
 export SSL_CERT_FILE=/usr/local/opt/curl-ca-bundle/share/ca-bundle.crt
 export GREP_COLOR='1;33'
-PATH="/Applications/Postgres93.app/Contents/MacOS/bin:$PATH"
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='fg=yellow,bold'
-fpath=(~/.zsh/Completions $fpath)
 
 # Settings {{{1
-setopt alwaystoend
-setopt menu_complete
-setopt prompt_subst
-setopt globdots
-setopt correct
-setopt pushdtohome
-setopt cdablevars
-setopt autolist
-setopt recexact
-setopt longlistjobs
-setopt autoresume
-setopt extendedhistory
-setopt histignoredups
-setopt histreduceblanks
-setopt histignorespace
-setopt pushdsilent
-setopt autopushd
-setopt pushdminus
-setopt extendedglob
-setopt rcquotes
-setopt globcomplete
-setopt rmstarwait
-setopt completeinword
-setopt completealiases
-setopt sharehistory
-setopt noclobber
-setopt markdirs
-setopt autoparamslash
-setopt chaselinks
-setopt combiningchars
-setopt multios
-KEYTIMEOUT=1
-
-# Key remappings {{{1
-bindkey -v
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
-bindkey '^N' insert-last-word
-bindkey '^L' clear-screen
+setopt alwaystoend completeinword completealiases pathdirs automenu autolist
+setopt prompt_subst extendedglob globdots globcomplete correct recexact
+setopt sharehistory histignoredups histreduceblanks histignorespace
+setopt banghist incappendhistory histexpiredupsfirst histignorealldups
+setopt histfindnodups histsavenodups histverify
+setopt pushdtohome pushdsilent autopushd pushdminus
+setopt longlistjobs autoresume multios
+setopt cdablevars multios rmstarwait
+setopt rcquotes autoparamslash markdirs
+setopt chaselinks combiningchars
+unsetopt flowcontrol caseglob clobber extendedhistory
 
 # History {{{1
-HISTFILE=$HOME/.zhistory
+HISTFILE=$HOME/.cache/.zhistory
 HISTSIZE=200000
 SAVEHIST=200000
 
-# Functions {{{1
-function :h-helper () {
-    vim + "h" +"h $1" +only +'nnoremap q :q!<CR>';
-}
+# Key remappings {{{1
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
