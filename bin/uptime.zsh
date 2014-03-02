6616689bcbee351b2623566909c88aeb46c1c832
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-02 00:49:48 -0800

    Fix spacing in script

--- bin/uptime.zsh
+++ bin/uptime.zsh
@@ -4,7 +4,7 @@
 UPTIME=`uptime | sed -e "s/up//" \
   | cut -f 4-14 -d " " | sed -e "s/[0-9]*:[0-9]*,//" \
   | sed -e "s/[0-9]* users,//" | sed -e "s/   //" \
-  | sed -e "s/averages://" | sed -e "s/load/| /" \
+  | sed -e "s/averages://" | sed -e "s/load/ | /" \
   | sed -e "s/  //" | sed -e "s/,//"`
 
 echo $UPTIME
