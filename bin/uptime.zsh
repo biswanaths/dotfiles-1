87515bbea0eacb4db0f582e5ee2d03e210f98cde
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-02 00:58:29 -0800

    Much better uptime and load averages script for tmux
    And I don't know a thing about awk. This is great!

--- bin/uptime.zsh
+++ bin/uptime.zsh
@@ -1,10 +1,4 @@
 #!/usr/local/bin/zsh
-# note: I know this is messy but it works.
+# Used in my tmux status line for uptime and load averages
 
-UPTIME=`uptime | sed -e "s/up//" \
-  | cut -f 4-14 -d " " | sed -e "s/[0-9]*:[0-9]*,//" \
-  | sed -e "s/[0-9]* users,//" | sed -e "s/   //" \
-  | sed -e "s/averages://" | sed -e "s/load/ | /" \
-  | sed -e "s/  //" | sed -e "s/,//"`
-
-echo $UPTIME
+echo `uptime | awk '{ print $3, $4, $10, $11, $12 }' | sed "s/,/ | /"`
