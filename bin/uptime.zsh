bc465fff2dd0187a3e996e32c86bfbbab21b2bdd
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-01 18:03:37 -0800

    Better uptime script for tmux

---
+++ bin/uptime.zsh
@@ -0,0 +1,6 @@
+#!/usr/local/bin/zsh
+# note: I know this is messy but it works.
+
+UPTIME=`uptime | sed -e "s/up//" | cut -f 4-14 -d " " | sed -e "s/[0-9]*:[0-9]*,//" | sed -e "s/[0-9]* users,//" | sed -e "s/   //" | sed -e "s/averages://" | sed -e "s/load/| /" | sed -e "s/  //" | sed -e "s/,//"`
+
+echo $UPTIME
