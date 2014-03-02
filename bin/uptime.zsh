01920ebea63c435d74061223bdabe0ab96c25739
Akshay Hegde <akshay.hegde17@gmail.com>
2014-03-01 21:23:01 -0800

    Format uptime script so it's actually readable

--- bin/uptime.zsh
+++ bin/uptime.zsh
@@ -1,6 +1,10 @@
 #!/usr/local/bin/zsh
 # note: I know this is messy but it works.
 
-UPTIME=`uptime | sed -e "s/up//" | cut -f 4-14 -d " " | sed -e "s/[0-9]*:[0-9]*,//" | sed -e "s/[0-9]* users,//" | sed -e "s/   //" | sed -e "s/averages://" | sed -e "s/load/| /" | sed -e "s/  //" | sed -e "s/,//"`
+UPTIME=`uptime | sed -e "s/up//" \
+  | cut -f 4-14 -d " " | sed -e "s/[0-9]*:[0-9]*,//" \
+  | sed -e "s/[0-9]* users,//" | sed -e "s/   //" \
+  | sed -e "s/averages://" | sed -e "s/load/| /" \
+  | sed -e "s/  //" | sed -e "s/,//"`
 
 echo $UPTIME
